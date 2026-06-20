9de620a655847f044b052507f84b7d6a99416bb9
Asim Abdulrahman Mohammed Omer <almhbob.iii@gmail.com>
2026-06-20 06:51:12 +0300

    register food routes

--- artifacts/api-server/src/routes/extra.ts
+++ artifacts/api-server/src/routes/extra.ts
@@ -1,1 +1,14 @@
-export { default, initStabilizationDb as initExtraDb } from "./stabilization";
+import { Router } from "express";
+import a, { initStabilizationDb } from "./stabilization";
+import b, { initFoodPosDb } from "./food-pos";
+
+const r = Router();
+r.use(a);
+r.use(b);
+
+export async function initExtraDb() {
+  await initStabilizationDb();
+  await initFoodPosDb();
+}
+
+export default r;
