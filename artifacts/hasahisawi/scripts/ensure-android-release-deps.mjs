c195cc7b652bf631bbcf2fc9810c5551e81d662e
Asim Abdulrahman Mohammed Omer <almhbob.iii@gmail.com>
2026-06-26 16:46:44 +0300

    add android deps helper

---
+++ artifacts/hasahisawi/scripts/ensure-android-release-deps.mjs
@@ -0,0 +1,9 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+
+const file = new URL('../package.json', import.meta.url);
+const pkg = JSON.parse(readFileSync(file, 'utf8'));
+pkg.dependencies ||= {};
+pkg.dependencies['react-native-qrcode-svg'] ||= '^6.3.15';
+pkg.dependencies['react-native-webview'] ||= '^13.16.0';
+writeFileSync(file, JSON.stringify(pkg, null, 2) + '\n');
+console.log('[ensure-android-release-deps] runtime Android dependencies ready');
